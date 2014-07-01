056ead805f13c180fc535a852dc57f883ac9030e
AE Creations <aecreations@server.fake>
2014-06-30 20:41:11 -0700

    Added new constants to support status bar icon; changed the API URL

--- src/modules/aeConstants.js
+++ src/modules/aeConstants.js
@@ -3,7 +3,15 @@ const EXPORTED_SYMBOLS = ["aeConstants"];
 
 
 var aeConstants = {
- DELICIOUS_API_URL: "https://api.del.icio.us/v1/",
+ DELICIOUS_API_URL: "https://delicious.com/v1/",
+
+ S4E_EXTENSION_ID: "status4evar@caligonstudios.com",
+ S4E_STATUSBAR_AREA_ID: "status4evar-status-bar",
+
+ // Status bar progress indicator statuses
+ SAVESTATUS_SAVING: "saving",
+ SAVESTATUS_SUCCESS: "success",
+ SAVESTATUS_FAILURE: "failure",
 
  // Result codes returned by the Delicious API
  RESULTCODE_DONE:                "done",
