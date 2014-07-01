095d3d68378eb533fce4f976c784c0e3a5bf2e9a
AE Creations <aecreations@server.fake>
2014-07-01 10:46:27 -0700

    Turned on debugging

--- src/modules/aeUtils.js
+++ src/modules/aeUtils.js
@@ -1,7 +1,7 @@
 
 const EXPORTED_SYMBOLS = ["aeUtils"];
 
-const DEBUG = false;
+const DEBUG = true;
 const EXTENSION_ID = "{d93e6838-8272-4382-a0fb-36a56db176c5}";
 
 const Cc = Components.classes;
