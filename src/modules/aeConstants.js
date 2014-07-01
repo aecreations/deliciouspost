7accb61e1b699352709dd870ff09ff189bbbde32
AE Creations <aecreations@server.fake>
2014-06-30 20:52:37 -0700

    Moved the status indicator icon delay value into a constant

--- src/modules/aeConstants.js
+++ src/modules/aeConstants.js
@@ -8,12 +8,15 @@ var aeConstants = {
  S4E_EXTENSION_ID: "status4evar@caligonstudios.com",
  S4E_STATUSBAR_AREA_ID: "status4evar-status-bar",
 
- // Status bar progress indicator statuses
+ // Status bar progress indicator statuses.
  SAVESTATUS_SAVING: "saving",
  SAVESTATUS_SUCCESS: "success",
  SAVESTATUS_FAILURE: "failure",
 
- // Result codes returned by the Delicious API
+ // Delay before the progress indicator status icon disappears.
+ PROGRESS_INDICATOR_DELAY: 4000,
+
+ // Result codes returned by the Delicious API.
  RESULTCODE_DONE:                "done",
  RESULTCODE_ITEM_ALREADY_EXISTS: "item already exists"
 };
